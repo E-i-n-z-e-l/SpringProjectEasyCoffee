04186ce16709480b0caca2af229b9cf11d04773a
Einzel <einzel0102@mail.ru>
2024-09-11 22:51:02 +0800

    "Пример проекта на Spring №2."

---
+++ SpringProject_02/src/main/java/com/example/SpringProject_02/SpringProject02Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringProject_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject02Application.class, args);
+	}
+
+}

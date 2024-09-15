8ca852a1effec3d3518d3fdf37b8afe7c932bd1d
Einzel <einzel0102@mail.ru>
2024-09-16 02:11:45 +0800

    "Пример создания простого веб-приложения с приветственным окном. Проведение тестирования."

---
+++ SpringProject_04/src/test/java/com/example/SpringProject_04/SpringProject04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringProject_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cff3f7223750a89550350a4bf4dc770cfd078abe
Einzel <einzel0102@mail.ru>
2024-09-21 23:50:41 +0800

    "Создание веб-приложения магазина с возможностью выбора ингредиентов."

--- SpringProject_03/src/main/java/com/example/SpringProject_03/Controller/DesignTacoController.java
+++ SpringProject_03/src/main/java/com/example/SpringProject_03/Controller/DesignTacoController.java
@@ -4,9 +4,11 @@ import com.example.SpringProject_03.Model.Ingredient;
 import com.example.SpringProject_03.Model.Ingredient.Type;
 import com.example.SpringProject_03.Model.Taco;
 import com.example.SpringProject_03.Model.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -59,7 +61,10 @@ public class DesignTacoController {
         return "design";
     }
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
